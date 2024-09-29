17a6c82b4940cf9357453a36ee87cba62e208a78
Pietro <pietrocalathaki@gmail.com>
2024-09-28 21:08:08 -0300

    IconButon para Frm Usuario

--- CapaPresentacion/FrmUsuario.Designer.cs
+++ CapaPresentacion/FrmUsuario.Designer.cs
@@ -39,6 +39,11 @@
             label5 = new Label();
             textBox5 = new TextBox();
             label6 = new Label();
+            label2 = new Label();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            label7 = new Label();
+            iconButton1 = new FontAwesome.Sharp.IconButton();
             SuspendLayout();
             // 
             // label1
@@ -47,13 +52,13 @@
             label1.Dock = DockStyle.Left;
             label1.Location = new Point(0, 0);
             label1.Name = "label1";
-            label1.Size = new Size(182, 479);
+            label1.Size = new Size(182, 471);
             label1.TabIndex = 0;
             // 
             // DNI
             // 
             DNI.AutoSize = true;
-            DNI.Location = new Point(12, 21);
+            DNI.Location = new Point(12, 9);
             DNI.Name = "DNI";
             DNI.Size = new Size(30, 15);
             DNI.TabIndex = 1;
@@ -62,14 +67,14 @@
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(12, 52);
+            textBox1.Location = new Point(12, 27);
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(149, 23);
             textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(12, 131);
+            textBox2.Location = new Point(12, 71);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(149, 23);
             textBox2.TabIndex = 5;
@@ -77,7 +82,7 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(12, 100);
+            label3.Location = new Point(12, 53);
             label3.Name = "label3";
             label3.Size = new Size(110, 15);
             label3.TabIndex = 4;
@@ -85,7 +90,7 @@
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(12, 206);
+            textBox3.Location = new Point(12, 115);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(149, 23);
             textBox3.TabIndex = 7;
@@ -93,7 +98,7 @@
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(12, 175);
+            label4.Location = new Point(12, 97);
             label4.Name = "label4";
             label4.Size = new Size(46, 15);
             label4.TabIndex = 6;
@@ -101,7 +106,7 @@
             // 
             // textBox4
             // 
-            textBox4.Location = new Point(12, 278);
+            textBox4.Location = new Point(12, 159);
             textBox4.Name = "textBox4";
             textBox4.Size = new Size(149, 23);
             textBox4.TabIndex = 9;
@@ -109,7 +114,7 @@
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(12, 247);
+            label5.Location = new Point(12, 141);
             label5.Name = "label5";
             label5.Size = new Size(70, 15);
             label5.TabIndex = 8;
@@ -117,7 +122,7 @@
             // 
             // textBox5
             // 
-            textBox5.Location = new Point(12, 347);
+            textBox5.Location = new Point(12, 203);
             textBox5.Name = "textBox5";
             textBox5.Size = new Size(149, 23);
             textBox5.TabIndex = 11;
@@ -125,18 +130,71 @@
             // label6
             // 
             label6.AutoSize = true;
-            label6.Location = new Point(12, 316);
+            label6.Location = new Point(12, 185);
             label6.Name = "label6";
             label6.Size = new Size(127, 15);
             label6.TabIndex = 10;
             label6.Text = "Confirmar Contraseña:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 229);
+            label2.Name = "label2";
+            label2.Size = new Size(27, 15);
+            label2.TabIndex = 12;
+            label2.Text = "Rol:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(12, 247);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(149, 23);
+            comboBox1.TabIndex = 13;
+            // 
+            // comboBox2
+            // 
+            comboBox2.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(12, 291);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(149, 23);
+            comboBox2.TabIndex = 15;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(12, 273);
+            label7.Name = "label7";
+            label7.Size = new Size(45, 15);
+            label7.TabIndex = 14;
+            label7.Text = "Estado:";
+            // 
+            // iconButton1
+            // 
+            iconButton1.IconChar = FontAwesome.Sharp.IconChar.None;
+            iconButton1.IconColor = Color.Black;
+            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton1.Location = new Point(27, 346);
+            iconButton1.Name = "iconButton1";
+            iconButton1.Size = new Size(75, 23);
+            iconButton1.TabIndex = 16;
+            iconButton1.Text = "iconButton1";
+            iconButton1.UseVisualStyleBackColor = true;
             // 
             // FrmUsuario
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(682, 479);
+            ClientSize = new Size(682, 471);
+            Controls.Add(iconButton1);
+            Controls.Add(comboBox2);
+            Controls.Add(label7);
+            Controls.Add(comboBox1);
+            Controls.Add(label2);
             Controls.Add(textBox5);
             Controls.Add(label6);
             Controls.Add(textBox4);
@@ -167,5 +225,10 @@
         private Label label5;
         private TextBox textBox5;
         private Label label6;
+        private Label label2;
+        private ComboBox comboBox1;
+        private ComboBox comboBox2;
+        private Label label7;
+        private FontAwesome.Sharp.IconButton iconButton1;
     }
 }

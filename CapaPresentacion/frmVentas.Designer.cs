c00de8f7b0829f958c3e3ea29888e4ea59d69783
Agustin-Cab <agustincab1915@gmail.com>
2024-09-30 10:46:45 -0300

    cambiado color de bg de frmVentas

--- CapaPresentacion/frmVentas.Designer.cs
+++ CapaPresentacion/frmVentas.Designer.cs
@@ -473,6 +473,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkSeaGreen;
             ClientSize = new Size(1141, 581);
             Controls.Add(btnregistrar);
             Controls.Add(txtcambio);

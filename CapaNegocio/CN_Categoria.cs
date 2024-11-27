1c62ad7888bae7bd69aad4430a0c3e5b13bdbb9f
Pietro <pietrocalathaki@gmail.com>
2024-11-27 18:56:58 -0300

    Capa negocio Categoria

--- CapaNegocio/CN_Categoria.cs
+++ CapaNegocio/CN_Categoria.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CapaDatos;
+using CapaEntidad;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,30 @@ using System.Threading.Tasks;
 
 namespace CapaNegocio
 {
-    internal class CN_Categoria
+    public class CN_Categoria
     {
+        private CD_Categoria objcd_Categoria = new CD_Categoria();
+        public List<Categoria> Listar()
+        {
+
+            return objcd_Categoria.Listar();
+        }
+        public int Registrar(Categoria obj, out string Mensaje)
+        {
+
+            return objcd_Categoria.Registrar(obj, out Mensaje);
+        }
+
+        public bool Editar(Categoria obj, out string Mensaje)
+        {
+
+            return objcd_Categoria.Editar(obj, out Mensaje);
+        }
+        public bool Eliminar(Categoria obj, out string Mensaje)
+        {
+
+            return objcd_Categoria.Eliminar(obj, out Mensaje);
+        }
     }
+}
 }

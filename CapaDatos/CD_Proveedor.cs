140683ab353abf7f77662cd6a06a6ce06ba68eec
Pietro <pietrocalathaki@gmail.com>
2025-10-04 10:14:19 -0300

    Provedor capa datos

--- CapaDatos/CD_Proveedor.cs
+++ CapaDatos/CD_Proveedor.cs
@@ -55,85 +55,81 @@ namespace CapaDatos
 
 
         public int Registrar(Proveedor obj, out string Mensaje)
+        {
+            int idusuariogenerado = 0;
+            Mensaje = string.Empty;
+            try
             {
-                int idusuariogenerado = 0;
-                Mensaje = string.Empty;
-                try
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
                 {
-                    using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
-                    {
-                        SqlCommand cmd = new SqlCommand("sp_RegistrarProveedor", oconexion);
-                        cmd.Parameters.AddWithValue("Documento", obj.Documento);
-                        cmd.Parameters.AddWithValue("RazonSocial", obj.RazonSocial);
-                        cmd.Parameters.AddWithValue("Correo", obj.Correo);
-                        cmd.Parameters.AddWithValue("Telefono", obj.Telefono);
-                        cmd.Parameters.AddWithValue("Estado", obj.Estado);
-                        cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
-                        cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
-                        cmd.CommandType = CommandType.StoredProcedure;
+                    SqlCommand cmd = new SqlCommand("sp_RegistrarProveedor", oconexion);
+                    cmd.Parameters.AddWithValue("Documento", obj.Documento);
+                    cmd.Parameters.AddWithValue("RazonSocial", obj.RazonSocial);
+                    cmd.Parameters.AddWithValue("Correo", obj.Correo);
+                    cmd.Parameters.AddWithValue("Telefono", obj.Telefono);
+                    cmd.Parameters.AddWithValue("Domicilio", obj.Domicilio); // 🔹 Agregar esta línea
+                    cmd.Parameters.AddWithValue("Estado", obj.Estado);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
 
-                        oconexion.Open();
+                    oconexion.Open();
+                    cmd.ExecuteNonQuery();
 
-                        cmd.ExecuteNonQuery();
-
-                        idusuariogenerado = Convert.ToInt32(cmd.Parameters["Resultado"].Value);
-                        Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
-
-
-                    }
-
+                    idusuariogenerado = Convert.ToInt32(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
                 }
-                catch (Exception ex)
-                {
-                    idusuariogenerado = 0;
-                    Mensaje = ex.Message;
-                }
-
-                return idusuariogenerado;
-
+            }
+            catch (Exception ex)
+            {
+                idusuariogenerado = 0;
+                Mensaje = ex.Message;
             }
 
-            public bool Editar(Proveedor obj, out string Mensaje)
+            return idusuariogenerado;
+        }
+
+
+        public bool Editar(Proveedor obj, out string Mensaje)
+        {
+            bool Resultado = false;
+            Mensaje = string.Empty;
+            try
             {
-                bool Resultado = false;
-                Mensaje = string.Empty;
-                try
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
                 {
-                    using (SqlConnection oconexion = new SqlConnection(Conexion.cadena))
-                    {
-                        SqlCommand cmd = new SqlCommand("sp_ModificarProveedor", oconexion);
-                        cmd.Parameters.AddWithValue("IdProveedor", obj.IdProveedor);
-                        cmd.Parameters.AddWithValue("Documento", obj.Documento);
-                        cmd.Parameters.AddWithValue("RazonSocial", obj.RazonSocial);
-                        cmd.Parameters.AddWithValue("Correo", obj.Correo);
-                        cmd.Parameters.AddWithValue("Telefono", obj.Telefono);
-                        cmd.Parameters.AddWithValue("Estado", obj.Estado);
-                        cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
-                        cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
-                        cmd.CommandType = CommandType.StoredProcedure;
+                    SqlCommand cmd = new SqlCommand("sp_ModificarProveedor", oconexion);
+                    cmd.Parameters.AddWithValue("IdProveedor", obj.IdProveedor);
+                    cmd.Parameters.AddWithValue("Documento", obj.Documento);
+                    cmd.Parameters.AddWithValue("RazonSocial", obj.RazonSocial);
+                    cmd.Parameters.AddWithValue("Correo", obj.Correo);
+                    cmd.Parameters.AddWithValue("Telefono", obj.Telefono);
+                    cmd.Parameters.AddWithValue("Domicilio", obj.Domicilio); // 🔹 nuevo parámetro
+                    cmd.Parameters.AddWithValue("Estado", obj.Estado);
 
-                        oconexion.Open();
+                    cmd.Parameters.Add("Resultado", SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
 
-                        cmd.ExecuteNonQuery();
+                    oconexion.Open();
 
-                        Resultado = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
-                        Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+                    cmd.ExecuteNonQuery();
 
-
-                    }
-
+                    Resultado = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    Mensaje = cmd.Parameters["Mensaje"].Value.ToString();
                 }
-                catch (Exception ex)
-                {
+            }
+            catch (Exception ex)
+            {
                 Resultado = false;
-                    Mensaje = ex.Message;
-                }
-
-                return Resultado;
-
+                Mensaje = ex.Message;
             }
 
-            public bool Eliminar(Proveedor obj, out string Mensaje)
+            return Resultado;
+        }
+
+
+        public bool Eliminar(Proveedor obj, out string Mensaje)
             {
                 bool Resultado = false;
                 Mensaje = string.Empty;

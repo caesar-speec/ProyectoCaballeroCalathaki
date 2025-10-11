a657345c52d6db2e101fd3ffb3243a382efa8921
Pietro <pietrocalathaki@gmail.com>
2025-10-11 16:52:31 -0300

    listar producto listar

--- CapaPresentacion/frmListarProductos.cs
+++ CapaPresentacion/frmListarProductos.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CapaEntidad;
+using CapaNegocio;
+using CapaPresentacion.Utilidades;
 
 namespace CapaPresentacion
 {
@@ -18,6 +21,41 @@ namespace CapaPresentacion
         }
 
         private void frmListarProductos_Load(object sender, EventArgs e)
+        {
+            //Mostrar usuarios
+            List<Producto> listaProducto = new CN_Producto().Listar();
+            foreach (Producto item in listaProducto)
+            {
+                dgvdata.Rows.Add(new object[] {
+                 "",
+                 item.IdProducto,
+                 item.Codigo,
+                 item.Nombre,
+
+                 item.Descripcion,
+                 item.oCategoria.IdCategoria,
+                 item.oCategoria.Descripcion,
+                 item.Stock,
+                 item.PrecioCompra,
+                 item.PrecioVenta,
+                 item.Estado == true ? 1 : 0,
+                 item.Estado == true ? "Activo" : "Inactivo"
+             });
+            }
+
+            foreach (DataGridViewColumn columna in dgvdata.Columns)
+            {
+                if (columna.Visible == true && columna.Name != "btnseleccionar")
+                {
+                    cbobusqueda.Items.Add(new OpcionCombo() { Valor = columna.Name, Texto = columna.HeaderText });
+                }
+            }
+            cbobusqueda.DisplayMember = "Texto";
+            cbobusqueda.ValueMember = "Valor";
+            cbobusqueda.SelectedIndex = 0;
+        }
+
+        private void dgvdata_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
